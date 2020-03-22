e824bef935306d8fccfdffb587a12aed95240d1e
yamasaki <yamasakikota@gmail.com>
2020-03-22 16:09:39 +0900

    Test4REFACTORING2

--- BoardGames/Assets/Scripts/Piece.cs
+++ BoardGames/Assets/Scripts/Piece.cs
@@ -14,9 +14,11 @@ public class Piece {
     }
 
     public void Work() {
-        if (Position == Cell.d3) GetNextPiece(Direction.down).Reverse();
-        if (Position == Cell.c5) GetNextPiece(Direction.right).Reverse();
-        if (Position == Cell.b6) GetNextPiece(Direction.upRight).Reverse();
+        foreach (var direction in DirectionExtend.AllCases) {
+            var nextPiece = GetNextPiece(direction);
+            if (nextPiece == null) continue;
+            nextPiece.Reverse();
+        }
     }
 
     void Reverse() {
