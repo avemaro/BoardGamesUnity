69c574306c26dfe2eff2d022719779dc2e72fc75
yamasaki <yamasakikota@gmail.com>
2020-03-22 10:38:32 +0900

    REFACTORING

--- BoardGames/Assets/Scripts/Board.cs
+++ BoardGames/Assets/Scripts/Board.cs
@@ -2,13 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Board
-{
-    public PieceColor GetColor(Cell cell) {
-        if (cell == Cell.d5) return PieceColor.black;
-        if (cell == Cell.e4) return PieceColor.black;
-        if (cell == Cell.d4) return PieceColor.white;
-        if (cell == Cell.e5) return PieceColor.white;
-        return PieceColor.none;
+public class Board {
+    List<Piece> pieces = new List<Piece>();
+
+    public Board() {
+        pieces.Add(new Piece(this, PieceColor.black, Cell.d5));
+        pieces.Add(new Piece(this, PieceColor.black, Cell.e4));
+        pieces.Add(new Piece(this, PieceColor.white, Cell.d4));
+        pieces.Add(new Piece(this, PieceColor.white, Cell.e5));
+    }
+
+    public PieceColor GetColor(Cell? cell) {
+        var piece = GetPiece(cell);
+        if (piece == null) return PieceColor.none;
+        return piece.Color;
+    }
+
+    Piece GetPiece(Cell? cell) {
+        if (cell == null) return null;
+        foreach (var piece in pieces)
+            if (piece.Position == cell) return piece;
+        return null;
     }
 }
--- BoardGames/Assets/Scripts/Piece.cs
+++ BoardGames/Assets/Scripts/Piece.cs
@@ -2,8 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Piece
-{
+public class Piece {
+    Board board;
+    public PieceColor Color { get; private set; }
+    public Cell Position { get; private set; }
+
+    public Piece(Board board, PieceColor color, Cell position) {
+        this.board = board;
+        Color = color;
+        Position = position;
+    }
 }
 
 public enum PieceColor {
