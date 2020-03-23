ef92157fa30246d4fa1fc725cd4dcada4161ce67
yamasaki <yamasakikota@gmail.com>
2020-03-23 21:42:23 +0900

    Test1_TEST

--- BoardGames/Assets/Tests/EditerTests/ReverseTest.cs
+++ BoardGames/Assets/Tests/EditerTests/ReverseTest.cs
@@ -8,5 +8,16 @@ namespace Tests
 {
     public class ReverseTest
     {
+        [Test]
+        public void Test1_BoardHasInitialized() {
+            var board = new Board((PieceColor.black, Cell.d5),
+                                  (PieceColor.black, Cell.e4),
+                                  (PieceColor.white, Cell.d4),
+                                  (PieceColor.white, Cell.e5));
+            Assert.AreEqual(PieceColor.black, board.GetColor(Cell.d5));
+            Assert.AreEqual(PieceColor.black, board.GetColor(Cell.e4));
+            Assert.AreEqual(PieceColor.white, board.GetColor(Cell.d4));
+            Assert.AreEqual(PieceColor.white, board.GetColor(Cell.e5));
+        }
     }
 }
