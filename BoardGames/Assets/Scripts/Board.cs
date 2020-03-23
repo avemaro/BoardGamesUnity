7863b14114fcd5a10aee339ff061d30d8f26070f
yamasaki <yamasakikota@gmail.com>
2020-03-23 23:41:31 +0900

    Test4_REFACTORING1

--- BoardGames/Assets/Scripts/Board.cs
+++ BoardGames/Assets/Scripts/Board.cs
@@ -4,26 +4,29 @@ using UnityEngine;
 
 public class Board
 {
-    List<(PieceColor color, Cell cell)> pieces;
+    List<Piece> pieces;
     public PieceColor ColorInTurn { get; private set; } = PieceColor.black;
 
     public Board() {
-        pieces = new List<(PieceColor, Cell)> {
-            (PieceColor.black, Cell.d5),
-            (PieceColor.black, Cell.e4),
-            (PieceColor.white, Cell.d4),
-            (PieceColor.white, Cell.e5)
+        pieces = new List<Piece> {
+            new Piece(PieceColor.black, Cell.d5),
+            new Piece(PieceColor.black, Cell.e4),
+            new Piece(PieceColor.white, Cell.d4),
+            new Piece(PieceColor.white, Cell.e5)
         };
     }
 
     public Board(params (PieceColor, Cell)[] pieces ) {
-        this.pieces = new List<(PieceColor color, Cell cell)>(pieces);
+        this.pieces = new List<Piece>();
+        foreach (var piece in pieces) {
+            this.pieces.Add(new Piece(piece.Item1, piece.Item2));
+        }
     }
 
     #region color
     public PieceColor GetColor(Cell cell) {
         foreach (var piece in pieces)
-            if (cell == piece.cell) return piece.color;
+            if (cell == piece.Position) return piece.Color;
         return PieceColor.none;
     }
     public bool IsNone(Cell cell) {
@@ -42,16 +45,15 @@ public class Board
 
         for (var i = 0; i < pieces.Count; i++) {
             var piece = pieces[i];
-            if (cell == Cell.d3 && piece.cell == Cell.d4)
-                pieces[i] = (PieceColor.black, Cell.d4);
-            if (cell == Cell.c5 && piece.cell == Cell.d5)
-                pieces[i] = (PieceColor.white, Cell.d5);
-            if (cell == Cell.b6 && piece.cell == Cell.c5)
-                pieces[i] = (PieceColor.black, Cell.c5);
+            if (cell == Cell.d3 && piece.Position == Cell.d4)
+                pieces[i] = new Piece(PieceColor.black, Cell.d4);
+            if (cell == Cell.c5 && piece.Position == Cell.d5)
+                pieces[i] = new Piece(PieceColor.white, Cell.d5);
+            if (cell == Cell.b6 && piece.Position == Cell.c5)
+                pieces[i] = new Piece(PieceColor.black, Cell.c5);
         }
 
-
-        pieces.Add((ColorInTurn, cell));
+        pieces.Add(new Piece(ColorInTurn, cell));
         ColorInTurn = ColorInTurn.Reversed();
         return true;
     }
