133f535c746258c787e5f0dabeb72045a2f9a339
yamasaki <yamasakikota@gmail.com>
2020-03-23 22:20:10 +0900

    Test1_RED

---
+++ BoardGames/Assets/Scripts/Board.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board
+{
+    public Board(params (PieceColor, Cell)[] pieces ) {
+
+    }
+
+    public PieceColor GetColor(Cell cell) {
+        return PieceColor.none;
+    }
+}
