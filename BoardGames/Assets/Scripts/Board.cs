771e8aeb56766039d9854837f8e82d141594e67b
yamasaki <yamasakikota@gmail.com>
2020-03-23 22:36:14 +0900

    Test2_RED

--- BoardGames/Assets/Scripts/Board.cs
+++ BoardGames/Assets/Scripts/Board.cs
@@ -5,6 +5,16 @@ using UnityEngine;
 public class Board
 {
     (PieceColor color, Cell cell)[] pieces;
+    public PieceColor ColorInTurn { get; private set; }
+
+    public Board() {
+        pieces = new (PieceColor, Cell)[] {
+            (PieceColor.black, Cell.d5),
+            (PieceColor.black, Cell.e4),
+            (PieceColor.white, Cell.d4),
+            (PieceColor.white, Cell.e5)
+        };
+    }
 
     public Board(params (PieceColor, Cell)[] pieces ) {
         this.pieces = pieces;
@@ -14,5 +24,8 @@ public class Board
         foreach (var piece in pieces)
             if (cell == piece.cell) return piece.color;
         return PieceColor.none;
+    }
+
+    public void PutPiece(Cell cell) {
     }
 }
