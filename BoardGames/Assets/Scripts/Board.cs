d48f55d74718b887bbd28f673f18834b9122bc49
avemaro <yamasakikota@gmail.com>
2020-03-24 21:27:51 +0900

    Test4_GREEN

--- BoardGames/Assets/Scripts/Board.cs
+++ BoardGames/Assets/Scripts/Board.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Board {
+    Dictionary<Cell, PieceColor> pieces = new Dictionary<Cell, PieceColor>();
     public PieceColor ColorInTurn { get; private set; } = PieceColor.black;
 
     public PieceColor GetColor(Cell cell) {
-        if (cell == Cell.d3) return PieceColor.black;
-        if (cell == Cell.c5) return PieceColor.white;
-        return PieceColor.none;
+        PieceColor color;
+        if (!pieces.TryGetValue(cell, out color)) return PieceColor.none;
+        return color;
     }
 
     public void PutPiece(Cell cell) {
+        if (cell == Cell.d3) pieces.Add(cell, PieceColor.black);
+        if (cell == Cell.c5) pieces.Add(cell, PieceColor.white);
+        if (ColorInTurn == PieceColor.black) ColorInTurn = PieceColor.white;
+        else if (ColorInTurn == PieceColor.white) ColorInTurn = PieceColor.black;
     }
 }
