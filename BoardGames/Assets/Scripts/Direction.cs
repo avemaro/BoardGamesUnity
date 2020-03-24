ef536a171d40f7e7c1904da4ef0a6af17b33f4e5
avemaro <yamasakikota@gmail.com>
2020-03-24 20:54:01 +0900

    Test1_2_GREEN

---
+++ BoardGames/Assets/Scripts/Direction.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Direction {
+    up = -8, down = 8, right = 1, left = -1,
+    upRight = -7, upLeft = -9, downRight = 9, downLeft = 7
+}
+
+public static class DirectionExtend {
+    public static Direction[] AllCases = (Direction[])Enum.GetValues(typeof(Direction));
+
+    public static (int x, int y) Pos(this Direction direction) {
+        switch (direction) {
+            case Direction.up: return (0, -1);
+            case Direction.upRight: return (1, -1);
+            case Direction.right: return (1, 0);
+            case Direction.downRight: return (1, 1);
+            case Direction.down: return (0, 1);
+            case Direction.downLeft: return (-1, 1);
+            case Direction.left: return (-1, 0);
+            case Direction.upLeft: return (-1, -1);
+        }
+        return (0, 0);
+    }
+
+    public static Direction Reverse(this Direction direction) {
+        switch (direction) {
+            case Direction.up: return Direction.down;
+            case Direction.upRight: return Direction.downLeft;
+            case Direction.right: return Direction.left;
+            case Direction.downRight: return Direction.upLeft;
+            case Direction.down: return Direction.up;
+            case Direction.downLeft: return Direction.upRight;
+            case Direction.left: return Direction.right;
+            default: return Direction.downRight;
+        }
+    }
+}
---
+++ BoardGames/Assets/Tests/EditerTests/GomokuTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class GomokuTests
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void Test1_a1HasNoCellOnUpAndLeft() {
+            var a1 = Cell.a1;
+            Assert.IsNull(a1.Next(Direction.up));
+            Assert.IsNull(a1.Next(Direction.upRight));
+            Assert.IsNotNull(a1.Next(Direction.right));
+            Assert.IsNotNull(a1.Next(Direction.downRight));
+            Assert.IsNotNull(a1.Next(Direction.down));
+            Assert.IsNull(a1.Next(Direction.downLeft));
+            Assert.IsNull(a1.Next(Direction.left));
+            Assert.IsNull(a1.Next(Direction.upLeft));
+        }
+
+        [Test]
+        public void Test2_h8HasNoCellOnUpAndLeft() {
+            var h8 = Cell.h8;
+            Assert.IsNotNull(h8.Next(Direction.up));
+            Assert.IsNull(h8.Next(Direction.upRight));
+            Assert.IsNull(h8.Next(Direction.right));
+            Assert.IsNull(h8.Next(Direction.downRight));
+            Assert.IsNull(h8.Next(Direction.down));
+            Assert.IsNull(h8.Next(Direction.downLeft));
+            Assert.IsNotNull(h8.Next(Direction.left));
+            Assert.IsNotNull(h8.Next(Direction.upLeft));
+        }
+    }
+}
