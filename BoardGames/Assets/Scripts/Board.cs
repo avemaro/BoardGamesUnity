fdf203b80ad085fe48fb1d58ca31ea50409bc9d3
avemaro <yamasakikota@gmail.com>
2020-03-24 21:14:23 +0900

    Test4_RED

--- BoardGames/Assets/Scripts/Board.cs
+++ BoardGames/Assets/Scripts/Board.cs
@@ -3,7 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Board {
+    public PieceColor ColorInTurn { get; private set; }
+
     public PieceColor GetColor(Cell cell) {
         return PieceColor.none;
+    }
+
+    public void PutPiece(Cell cell) {
+
     }
 }
