50a44c8e65b2407dcec41bbb9a3d5ad0046f6710
avemaro <yamasakikota@gmail.com>
2020-03-24 21:46:38 +0900

    Test6_RED

--- BoardGames/Assets/Scripts/Board.cs
+++ BoardGames/Assets/Scripts/Board.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Board {
     Dictionary<Cell, PieceColor> pieces = new Dictionary<Cell, PieceColor>();
     public PieceColor ColorInTurn { get; private set; } = PieceColor.black;
+    public bool IsGameOver { get; private set; }
+    public PieceColor Winner { get; private set; } 
 
     public PieceColor GetColor(Cell cell) {
         pieces.TryGetValue(cell, out PieceColor color);
