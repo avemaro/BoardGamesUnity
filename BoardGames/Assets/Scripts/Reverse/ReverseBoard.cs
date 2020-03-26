9a6e13f88467156381864ac263f88988e2074bb1
avemaro <yamasakikota@gmail.com>
2020-03-26 22:24:56 +0900

    Test3_REFACTORING4

--- BoardGames/Assets/Scripts/Reverse/ReverseBoard.cs
+++ BoardGames/Assets/Scripts/Reverse/ReverseBoard.cs
@@ -10,18 +10,21 @@ public class ReverseBoard : Board {
         pieces.Add(new ReversePiece(this, PieceColor.white, Cell.e5));
     }
 
-    protected override bool IsRegal(Piece newPiece) {
-        if (!base.IsRegal(newPiece)) return false;
+    public override bool PutPiece(Cell cell) {
+        var newPiece = new ReversePiece(this, ColorInTurn, cell);
 
-        foreach (var direction in DirectionExtend.AllCases) {
-            var nextCell = newPiece.Position.Next(direction);
-            if (nextCell == null) continue;
-            if (GetColor((Cell)nextCell) != ColorInTurn.Reverse()) continue;
-            var next2Cell = ((Cell)nextCell).Next(direction);
-            if (next2Cell == null) continue;
-            if (GetColor((Cell)next2Cell) != ColorInTurn) continue;
-            return true;
-        }
-        return false;
+        if (!IsRegal(newPiece)) return false;
+        pieces.Add(newPiece);
+        newPiece.Work();
+
+        DecideWinner();
+        ColorInTurn = ColorInTurn.Reverse();
+
+        return true;
+    }
+
+    protected bool IsRegal(ReversePiece newPiece) {
+        if (!base.IsRegal(newPiece)) return false;
+        return newPiece.CheckReversible();
     }
 }
--- BoardGames/Assets/Scripts/Reverse/ReversePiece.cs
+++ BoardGames/Assets/Scripts/Reverse/ReversePiece.cs
@@ -6,4 +6,17 @@ public class ReversePiece : Piece {
     public ReversePiece(Board board, PieceColor color, Cell position)
         : base(board, color, position) {
     }
+
+    public bool CheckReversible() {
+        foreach (var direction in DirectionExtend.AllCases) {
+            var nextCell = Position.Next(direction);
+            if (nextCell == null) continue;
+            if (board.GetColor((Cell)nextCell) != Color.Reverse()) continue;
+            var next2Cell = ((Cell)nextCell).Next(direction);
+            if (next2Cell == null) continue;
+            if (board.GetColor((Cell)next2Cell) != Color) continue;
+            return true;
+        }
+        return false;
+    }
 }
