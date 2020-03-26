42921da43a8bc24afdead41b0ee53a66fdac7680
avemaro <yamasakikota@gmail.com>
2020-03-26 22:04:51 +0900

    Test3_GREEN

--- BoardGames/Assets/Scripts/Reverse/ReverseBoard.cs
+++ BoardGames/Assets/Scripts/Reverse/ReverseBoard.cs
@@ -9,4 +9,17 @@ public class ReverseBoard : Board {
         pieces.Add(new ReversePiece(this, PieceColor.white, Cell.d4));
         pieces.Add(new ReversePiece(this, PieceColor.white, Cell.e5));
     }
+
+    protected override bool IsRegal(Piece newPiece) {
+        if (!base.IsRegal(newPiece)) return false;
+
+        if (ColorInTurn == PieceColor.white) return true;
+
+        if (newPiece.Position == Cell.c4 ||
+            newPiece.Position == Cell.d3 ||
+            newPiece.Position == Cell.e6 ||
+            newPiece.Position == Cell.f5)
+            return true;
+        return false;
+    }
 }
