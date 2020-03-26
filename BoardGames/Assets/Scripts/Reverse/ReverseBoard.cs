fcd25249917b14c73eb56bb8beae574aac6e0f1b
avemaro <yamasakikota@gmail.com>
2020-03-26 20:18:16 +0900

    Test3_RED3

--- BoardGames/Assets/Scripts/Reverse/ReverseBoard.cs
+++ BoardGames/Assets/Scripts/Reverse/ReverseBoard.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class ReverseBoard : Board {
     public ReverseBoard() {
-        PutPiece(Cell.d5);
-        PutPiece(Cell.d4);
-        PutPiece(Cell.e4);
-        PutPiece(Cell.e5);
+        pieces.Add(new Piece(this, PieceColor.black, Cell.d5));
+        pieces.Add(new Piece(this, PieceColor.black, Cell.e4));
+        pieces.Add(new Piece(this, PieceColor.white, Cell.d4));
+        pieces.Add(new Piece(this, PieceColor.white, Cell.e5));
     }
 
     public override bool PutPiece(Cell cell) {
+
+
         if (cell == Cell.c4 || cell == Cell.d3 ||
             cell == Cell.e6 || cell == Cell.f5)
             return base.PutPiece(cell);
