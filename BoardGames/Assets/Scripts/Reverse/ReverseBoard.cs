7b46df76a01035b3a8adc75e06bf2ec74765ba8b
avemaro <yamasakikota@gmail.com>
2020-03-26 20:00:28 +0900

    Test1_GREEN

--- BoardGames/Assets/Scripts/Reverse/ReverseBoard.cs
+++ BoardGames/Assets/Scripts/Reverse/ReverseBoard.cs
@@ -3,5 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ReverseBoard : Board {
-
+    public ReverseBoard() {
+        PutPiece(Cell.d5);
+        PutPiece(Cell.d4);
+        PutPiece(Cell.e4);
+        PutPiece(Cell.e5);
+    }
 }
