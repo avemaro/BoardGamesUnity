ab642143f41a9910da180eb97638fe576186becd
avemaro <yamasakikota@gmail.com>
2020-03-26 22:10:53 +0900

    Test3_REFACTORING3

--- BoardGames/Assets/Scripts/Reverse/ReverseBoard.cs
+++ BoardGames/Assets/Scripts/Reverse/ReverseBoard.cs
@@ -13,17 +13,15 @@ public class ReverseBoard : Board {
     protected override bool IsRegal(Piece newPiece) {
         if (!base.IsRegal(newPiece)) return false;
 
-        if (ColorInTurn == PieceColor.white) return true;
-
-        var cell = newPiece.Position;
-        if (cell == Cell.c4 && IsWhite(Cell.d4) && IsBlack(Cell.e4))
+        foreach (var direction in DirectionExtend.AllCases) {
+            var nextCell = newPiece.Position.Next(direction);
+            if (nextCell == null) continue;
+            if (GetColor((Cell)nextCell) != ColorInTurn.Reverse()) continue;
+            var next2Cell = ((Cell)nextCell).Next(direction);
+            if (next2Cell == null) continue;
+            if (GetColor((Cell)next2Cell) != ColorInTurn) continue;
             return true;
-        if (cell == Cell.d3 && IsWhite(Cell.d4) && IsBlack(Cell.d5))
-            return true;
-        if (cell == Cell.e6 && IsWhite(Cell.e5) && IsBlack(Cell.e4))
-            return true;
-        if (cell == Cell.f5 && IsWhite(Cell.e5) && IsBlack(Cell.d5))
-            return true;
+        }
         return false;
     }
 }
