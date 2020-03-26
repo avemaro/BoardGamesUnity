c53f2f5167a9c557cd2762c4c57cde0e1a302267
avemaro <yamasakikota@gmail.com>
2020-03-26 19:56:28 +0900

    Test1_TEST

---
+++ BoardGames/Assets/Tests/EditerTests/ReverseTests.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class ReverseTests {
+        [Test]
+        public void Test1_BoardHasInitialized() {
+            var board = new ReverseBoard();
+            Assert.AreEqual(PieceColor.black, board.GetColor(Cell.d5));
+            Assert.AreEqual(PieceColor.black, board.GetColor(Cell.e4));
+            Assert.AreEqual(PieceColor.white, board.GetColor(Cell.d4));
+            Assert.AreEqual(PieceColor.white, board.GetColor(Cell.e5));
+        }
+    }
+}
