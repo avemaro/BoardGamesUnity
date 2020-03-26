77e4d22ef02b2ea04c617cad0cce9d962b82f621
avemaro <yamasakikota@gmail.com>
2020-03-26 22:44:06 +0900

    Test4_RED

--- BoardGames/Assets/Tests/EditerTests/ReverseTests.cs
+++ BoardGames/Assets/Tests/EditerTests/ReverseTests.cs
@@ -39,5 +39,16 @@ namespace Tests
                 Assert.False(board.PutPiece(cell));
             }
         }
+
+        [Test]
+        public void Test4_APieceHasReveresed() {
+            var board = new ReverseBoard();
+            Assert.True(board.PutPiece(Cell.d3));
+            Assert.True(board.IsBlack(Cell.d3));
+            Assert.True(board.IsBlack(Cell.d4));
+            Assert.True(board.PutPiece(Cell.c5));
+            Assert.True(board.IsWhite(Cell.c5));
+            Assert.True(board.IsWhite(Cell.d5));
+        }
     }
 }
