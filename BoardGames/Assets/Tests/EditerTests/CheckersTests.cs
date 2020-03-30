bd7c600ebc281d260f38878a7281ca8f850e5887
avemaro <yamasakikota@gmail.com>
2020-03-30 20:19:22 +0900

    Test1_TEST

---
+++ BoardGames/Assets/Tests/EditerTests/CheckersTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class CheckersTests
+    {
+        [Test]
+        public void Test1_BoardHasInitialized()
+        {
+            var board = CheckersBoard();
+            var blackCells = new Cell[] {Cell.a6, Cell.a8, Cell.b7,
+                                         Cell.c6, Cell.c8, Cell.d7,
+                                         Cell.e6, Cell.e8, Cell.f7,
+                                         Cell.g6, Cell.g8, Cell.h7};
+            var whiteCells = new Cell[] {Cell.a2, Cell.b1, Cell.b3,
+                                         Cell.c2, Cell.d1, Cell.d3,
+                                         Cell.e2, Cell.f1, Cell.f3,
+                                         Cell.g2, Cell.h1, Cell.h3};
+            Assert.True(board.Chech(blackCells, whiteCells));
+        }
+    }
+}
