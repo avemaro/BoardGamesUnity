4092a0adcd321ef91ea946527e8671c4420237d8
avemaro <yamasakikota@gmail.com>
2020-03-30 22:11:32 +0900

    Revert "Test2_RED"
    This reverts commit 3395f59f62bc77f2c442c0e80b1a765918a0ced7.

--- BoardGames/Assets/Scripts/Board.cs
+++ BoardGames/Assets/Scripts/Board.cs
@@ -8,11 +8,10 @@ public abstract class Board {
     public bool IsGameOver { get; protected set; }
     public PieceColor Winner { get; protected set; }
 
-    public bool MovePiece(Cell from, Cell to) {
-        return false;
-        //var piece = GetPiece(from);
-        //return piece.Move(to);
-    }
+    //public bool MovePiece(Cell from, Cell to) {
+    //    var piece = GetPiece(from);
+    //    return piece.Move(to);
+    //}
 
     public bool PutPiece(Cell cell) {
         var newPiece = CreatePiece(cell);
