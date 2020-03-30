680ccd0adcdd5a6c0e81b195b77aa3d0f1053901
avemaro <yamasakikota@gmail.com>
2020-03-30 20:23:18 +0900

    Test1_GREEN

--- BoardGames/Assets/Scripts/Checkers/CheckersBoard.cs
+++ BoardGames/Assets/Scripts/Checkers/CheckersBoard.cs
@@ -3,6 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CheckersBoard : Board {
+    public CheckersBoard() {
+        var blackCells = new Cell[] {Cell.a6, Cell.a8, Cell.b7,
+                                         Cell.c6, Cell.c8, Cell.d7,
+                                         Cell.e6, Cell.e8, Cell.f7,
+                                         Cell.g6, Cell.g8, Cell.h7};
+        var whiteCells = new Cell[] {Cell.a2, Cell.b1, Cell.b3,
+                                         Cell.c2, Cell.d1, Cell.d3,
+                                         Cell.e2, Cell.f1, Cell.f3,
+                                         Cell.g2, Cell.h1, Cell.h3};
+        foreach (var cell in blackCells)
+            pieces.Add(new CheckersPiece(this, PieceColor.black, cell));
+        foreach (var cell in whiteCells)
+            pieces.Add(new CheckersPiece(this, PieceColor.white, cell));
+
+    }
+
     protected override Piece CreatePiece(PieceColor color, Cell cell) {
         throw new System.NotImplementedException();
     }
---
+++ BoardGames/Assets/Scripts/Checkers/CheckersPiece.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckersPiece : Piece {
+    public CheckersPiece(Board board, PieceColor color, Cell position) : base(board, color, position) {
+    }
+
+    public override void Work() {
+        throw new System.NotImplementedException();
+    }
+}
