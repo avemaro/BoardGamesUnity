6d168f7a4a170ac16b41f886a94e259e0e0eb0df
avemaro <yamasakikota@gmail.com>
2020-03-31 21:01:10 +0900

    Test2_REFACTORING4

--- BoardGames/Assets/Scripts/Checkers/CheckersPiece.cs
+++ BoardGames/Assets/Scripts/Checkers/CheckersPiece.cs
@@ -3,6 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CheckersPiece : Piece {
+    List<Direction> MoveDirections {
+        get {
+            if (Color == PieceColor.black)
+                return new List<Direction> { Direction.upLeft, Direction.upRight };
+            return new List<Direction> { Direction.downLeft, Direction.downRight };
+        }
+    }
+
     public CheckersPiece(Board board, PieceColor color, Cell position) : base(board, color, position) {
     }
 
@@ -12,8 +20,8 @@ public class CheckersPiece : Piece {
 
     public override bool IsRegal(Cell to) {
         if (board.GetPiece(to) != null) return false;
-        if (Color == PieceColor.black && to == Position.Next(Direction.upRight)) return true;
-        if (Color == PieceColor.white && to == Position.Next(Direction.downLeft)) return true;
+        foreach (var direction in MoveDirections)
+            if (to == Position.Next(direction)) return true;
         return false;
     }
 }
