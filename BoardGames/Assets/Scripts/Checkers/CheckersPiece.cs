b9a352943e33042a3dd1eb73847402a798938cdd
avemaro <yamasakikota@gmail.com>
2020-03-31 22:44:29 +0900

    Test5_REFACTORING1

--- BoardGames/Assets/Scripts/Checkers/CheckersPiece.cs
+++ BoardGames/Assets/Scripts/Checkers/CheckersPiece.cs
@@ -14,6 +14,17 @@ public class CheckersPiece : Piece {
     public CheckersPiece(Board board, PieceColor color, Cell position) : base(board, color, position) {
     }
 
+    bool CanCapture(Cell to) {
+        foreach (var direction in MoveDirections) {
+            var nextPiece = GetNextPiece(direction);
+            if (nextPiece == null) continue;
+            var next2Cell = nextPiece.Position.Next(direction);
+            if (to != next2Cell) continue;
+            return true;
+        }
+        return false;
+    }
+
     public override void Capture(Cell to) {
         var direction = DirectionExtend.GetDirection(Position, to);
         if (direction == null) return;
@@ -33,19 +44,13 @@ public class CheckersPiece : Piece {
     public override bool IsRegal(Cell to) {
         if (board.GetPiece(to) != null) return false;
 
+        if (CanCapture(to)) return true;
+
         if (Position == Cell.g6) return false;
         if (Position == Cell.d7) return false;
 
         foreach (var direction in MoveDirections)
             if (to == Position.Next(direction)) return true;
-
-        foreach (var direction in MoveDirections) {
-            var nextPiece = GetNextPiece(direction);
-            if (nextPiece == null) continue;
-            var next2Cell = nextPiece.Position.Next(direction);
-            if (to != next2Cell) continue;
-            return true;
-        }
 
         return false;
     }
